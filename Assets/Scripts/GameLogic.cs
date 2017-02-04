95d1cfc88b0d37045c8133ff069b496b5d8b9294
Mike <mi.tamvak@gmail.com>
2017-02-04 20:52:28 +0200

    README.md added

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -5,14 +5,21 @@ using UnityEngine.EventSystems;
 
 public class GameLogic : MonoBehaviour {
 
+  public float distanceVideoToCam = 10.0f;
+  public GameObject player;
   public GameObject museum;
-  public GameObject videoVRDrivingExperience;
-  public GameObject videoVRExperience;
-  public GameObject videoVREngineering;
-  public GameObject videoVRCustomerPreview;
-  public GameObject videoVRAirbrush;
+  public GameObject startPoint;
+  public GameObject restartPoint;
+  public GameObject welcomeScreen;
+  public GameObject videoVRDrivingExperience = null;
+  public GameObject videoVRExperience = null;
+  public GameObject videoVREngineering = null;
+  public GameObject videoVRCustomerPreview = null;
+  public GameObject videoVRAirbrush = null;
 
-  private GameObject video;
+  public AudioSource environmenMusic;
+
+  private GameObject video = null;
 
   // Use this for initialization
   void Start () {
@@ -30,23 +37,49 @@ public class GameLogic : MonoBehaviour {
     // Checks which video to play
     RaycastHit seen;
     Ray raydirection = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-    if (Physics.Raycast(raydirection, out seen, sightlength)) {
+    if (Physics.Raycast(raydirection, out seen, sightlength)) { 
+
+      // VERY IMPORTANT: Checks if the user stands still to create the video canvas, cause if he is still moving the 
+      // collider he has clicked is of the Waypoint. This causes to crash the app.
+      if(seen.collider.name == "Waypoint") {
+        Debug.Log("ERROR: The user is still moving");
+        return;
+      }
+
+      // Calculate the position and the rotation of the gameobject to instatiate it in front of the camera
+      Vector3 objPosition = Camera.main.transform.position + Camera.main.transform.forward * distanceVideoToCam;
+      Quaternion objRotation = new Quaternion(0.0f, Camera.main.transform.rotation.y, 0.0f, Camera.main.transform.rotation.w);
+
       museum.SetActive(false);    // Hide the museum
 
-      Vector3 p = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, -10));
-      Camera.main.transform.Rotate(p);
-
+      environmenMusic.Pause();    // Pauses the environment music
+      
+      // Checks which one of the videos it will play
       if (seen.collider.name == "HMD_1") {
-
+        if (videoVRDrivingExperience == null)
+          Debug.Log("Video canvas of VR Experience is missing");
+        else 
+          video = Instantiate(videoVRDrivingExperience, new Vector3(objPosition.x, 1.0f, objPosition.z), objRotation);
       } else if (seen.collider.name == "HMD_2") {
-        video = Instantiate(videoVRExperience, p, Quaternion.identity);
-        
+        if (videoVRExperience == null)
+          Debug.Log("Video canvas of VR Experience is missing");
+        else
+          video = Instantiate(videoVRExperience, new Vector3(objPosition.x, 2.0f, objPosition.z), objRotation);
       } else if (seen.collider.name == "HMD_3") {
-
+        if (videoVREngineering == null)
+          Debug.Log("Video canvas of VR Experience is missing");
+        else
+          video = Instantiate(videoVREngineering, new Vector3(objPosition.x, 5.0f, objPosition.z), objRotation);
       } else if (seen.collider.name == "HMD_4") {
-
+        if (videoVRCustomerPreview == null)
+          Debug.Log("Video canvas of VR Experience is missing");
+        else
+          video = Instantiate(videoVRCustomerPreview, new Vector3(objPosition.x, 4.0f, objPosition.z), objRotation);
       } else if (seen.collider.name == "HMD_5") {
-
+        if (videoVRAirbrush == null)
+          Debug.Log("Video canvas of VR Experience is missing");
+        else
+          video = Instantiate(videoVRAirbrush, new Vector3(objPosition.x, 5.0f, objPosition.z), objRotation);
       }
 
       // This adds at last an event trigger to exit the video when the user clicks on it
@@ -63,5 +96,23 @@ public class GameLogic : MonoBehaviour {
     museum.SetActive(true);    // Unhide the museum
     if(video != null) 
       Destroy(video);
+    environmenMusic.Play();   // Continues playing the environment music
+  }
+
+  // Start the sightseeing
+  public void OnClickStart() {
+    welcomeScreen.SetActive(false);
+    iTween.MoveTo(player, startPoint.transform.position, 15f);  // Moves to the signpost
+  }
+
+  // Restart the app
+  public void OnClickRestart() {
+    iTween.MoveTo(player, restartPoint.transform.position, 0.01f);  // Moves to the restart point
+    welcomeScreen.SetActive(true);
+  }
+
+  // Exit application
+  public void OnClickExitApplication() {
+    Application.Quit();
   }
 }
---
+++ README.md
@@ -0,0 +1,87 @@
+#Michail’s Night At The Museum Project
+
+As an exercise of an Udacity project, I have created a mobile VR application which is called NightAtTheMuseum. 
+The creation process included the design, iteration through the development process and the user testing. After 
+starting the application the user is located in a virtual car museum in front of a welcome screen. The screen 
+explains the contents of the application and how the movement and interactions occur. The user can go throu 
+the museum and visit stations that explain the respective VR technology in the automobile industry.
+
+###Considerations
+There were a lot of design concepts to include in this project, but one of the most important was to create an 
+atmospheric environment in which the user feels the mood of a virtual museum.
+
+##Night At The Museum
+Users who have tested the application were very impressed with the big environment and the different kinds of VR 
+technologies the museum introduces. In addition, the users were asked how they feel the speed of the navigation 
+and if it makes them sick. All users had no experience with VR applications even on mobile phones nor on pc. 
+
+
+####Video
+
+
+
+####Screenshots
+[Imgur](http://i.imgur.com/Gigbpy5.jpg)
+
+
+##Process
+**Statement of Purpose:** NightAtTheMuseum is a mobile VR application that gives people a different way to visit 
+a car museum.
+
+###Persona
+[Imgur](http://i.imgur.com/YhYwjFh.gif)
+
+*The user persona tat was created for this project is called John. He is 48 years old and works in a museum as guidance. 
+John is not married and spends his free time by going swimming at the nearest swimming pool. In addition he likes reading 
+books about inovative technologies. He has no experience with VR.*
+
+##Environment
+There were no sketches for the environment drawn cause of that a prefab of a museum from the asset store was used.
+
+##User Testing
+
+###Testing the scene and mood
+
+To test the VR application I conducted a user who had no experience with VR. At first, I tried to find out how big he felt in 
+the museum, how the diference of is height to the next floor was and he felt the mood in the environment. The user felt his 
+height was appropriate to the environment. He liked the relaxed music playing in the museum.
+
+###Testing the movement and motion sickness    
+
+I tried to know how the user felt about the movement speed and if he encountered any motion sickness at all. The user mentioned 
+that he didn’t encounter any sickness. In addition, I asked him if he felt any height changing or disorientation in the movement,
+he didn't.
+
+
+###Testing the whole application from beginning to the end
+
+The user said that in future there could be more VR stations to show additional technologies that will come. He felt no motion
+sickness and problems by navigating through the museum.
+
+
+###Testing the UIs
+
+I asked him how the UI's were accessible and how he felt the interaction with them. He had nothing to complain about. 
+
+##Breakdown of the Final Design
+
+The steps for the final design included the building of all description UIs and editing the video files for better displaying.
+
+##Conclusion
+
+This sort of visiting a virtual car museum gave me a big challenge to implement new things like displaying a video on a canvas
+without any or small delay by changing from frame to frame. It gave me a lot of fun to design and program the behavior.
+I hope that this kind of application has inspired the users to test more VR apps in future.
+
+##Next steps
+
+In future there could be more floors that show more inovative VR technologies in the automobile industries.
+
+
+
+
+
+
+
+
+
